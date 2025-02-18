f15091065e80eb8a84f31b1bfafd82cbd805a39e
IrinaKhrol <120750891+IrinaKhrol@users.noreply.github.com>
2025-02-18 12:30:36 +0200

    Update Function.cs

--- task02/dnapp/lambdas/hello_world/Function.cs
+++ task02/dnapp/lambdas/hello_world/Function.cs
@@ -8,49 +8,58 @@ using System.Text.Json;
 
 namespace SimpleLambdaFunction
 {
+   public class HelloResponse
+   {
+       public int statusCode { get; set; }
+       public string message { get; set; } = string.Empty;
+   }
 
-    public class HelloResponse
-    {
-        public int statusCode { get; set; }
-        public string message { get; set; } = string.Empty;
+   public class Function
+   {
+       public APIGatewayHttpApiV2ProxyResponse FunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+       {
+            context.Logger.LogLine($"Request path: {request.RequestContext.Http.Path}");
+            context.Logger.LogLine($"HTTP method: {request.RequestContext.Http.Method}");
 
-    }
+           if (request.RequestContext.Http.Path.EndsWith("/hello"))
+           {
+               var response = new HelloResponse
+               {
+                   statusCode = 200,
+                   message = "Hello from Lambda"
+               };
 
-    public class Function
-    {
-        public APIGatewayHttpApiV2ProxyResponse FunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
-        {
-
-            if (request.RequestContext.Http.Path.EndsWith("/hello"))
-            {
-                var response = new HelloResponse
-                {
-                    statusCode = 200,
-                    message = "Hello from Lambda"
-                };
-
-                var options = new JsonSerializerOptions
-                {
+               var options = new JsonSerializerOptions
+               {
                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-                };
+               };
 
-                return new APIGatewayHttpApiV2ProxyResponse
-                {
-                    StatusCode = 200,
-                    Body = JsonSerializer.Serialize(response),
-                    Headers = new Dictionary<string, string> 
-                    { 
-                        { "Content-Type", "application/json" } 
-                    }
-                };
-            }
+               return new APIGatewayHttpApiV2ProxyResponse
+               {
+                   StatusCode = 200,
+                   Body = JsonSerializer.Serialize(response, options),
+                   Headers = new Dictionary<string, string> 
+                   { 
+                       { "Content-Type", "application/json" } 
+                   }
+               };
+           }
 
-            return new APIGatewayHttpApiV2ProxyResponse
+            var errorResponse = new HelloResponse
             {
-                StatusCode = 404,
-                Body = "Not Found"
+                statusCode = 400,
+                message = $"Bad request syntax or unsupported method. Request path: {request.RequestContext.Http.Path}. HTTP method: {request.RequestContext.Http.Method}"
             };
 
-        }
-    }
+           return new APIGatewayHttpApiV2ProxyResponse
+           {
+                StatusCode = 400,
+                Body = JsonSerializer.Serialize(errorResponse, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase }),
+                Headers = new Dictionary<string, string>
+                {
+                    { "Content-Type", "application/json" }
+                }
+           };
+       }
+   }
 }
